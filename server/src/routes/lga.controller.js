2597250cd15ecaac42a0810c53d17a4fd0cadee3
Justin Evans <eustin.jvans@gmail.com>
2022-06-24 14:52:25 +1000

    server: call getLGA model with lat and lon

--- server/src/routes/lga.controller.js
+++ server/src/routes/lga.controller.js
@@ -18,7 +18,7 @@ async function pyGetLGA(req, res) {
   }
 
   try {
-    const result = await getLGA();
+    const result = await getLGA(lat, lon);
     return res.status(200).json({ lga: result });
   } catch (err) {
     return res.status(500).json({ error: "Error getting LGA" });
