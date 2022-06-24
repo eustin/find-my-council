35ce9249baf1636ee26bd6085d961ff54cc7a3e3
Justin Evans <eustin.jvans@gmail.com>
2022-06-24 12:45:09 +1000

    server: mount LGA router in express app.

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
+
+const lgaRouter = require("./routes/lga.router");
+app.use("/lga", lgaRouter);
 
 module.exports = app;
