eaae4e824ad0ec1d7098edf3ae63393204894eba
Justin Evans <eustin.jvans@gmail.com>
2022-06-24 14:52:04 +1000

    server: run python script in lga.model.js

--- server/src/models/lga.model.js
+++ server/src/models/lga.model.js
@@ -1,7 +1,22 @@
-const child_process = require("child_process");
+const { spawn } = require("child_process");
+const { once } = require("events");
+const path = require("path");
 
 async function getLGA(lat, lon) {
-  return "Sydney";
+  let result;
+  const scriptPath = path.join(__dirname, "..", "..", "scripts", "find_lga.py");
+  const python = spawn("python3", [scriptPath, lat, lon]);
+
+  python.stdout.on("data", (data) => {
+    result = data.toString();
+  });
+
+  python.stderr.on("data", (data) => {
+    console.log(`error: ${data}`);
+  });
+
+  await once(python, "close");
+  return result;
 }
 
 module.exports = getLGA;
